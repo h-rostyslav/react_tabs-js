6cef94b35ee8c814a4f4174a50b4690ca86e9c98
Rostyslav` <hrostyslav@ukr.net`>
2026-06-23 14:59:06 +0300

    task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
-  import React, { useState } from 'react';
-  import 'bulma/css/bulma.css';
-  import '@fortawesome/fontawesome-free/css/all.css';
-  import './App.scss';
-  import { Tabs } from './components/Tabs/Tabs';
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
-  export const tabs = [
-    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-  ];
+export const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-  export const App = () => {
-    const [activeTabId, setActiveTabId] = useState(tabs[0].id);
-    const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+export const App = () => {
+  const [activeTabId, setActiveTabId] = useState(tabs[0].id);
 
-    return (
-      <div className="section">
-        <h1 className="title">Selected tab is {activeTab.title}</h1>
-
-        <div data-cy="TabsComponent">
-          <Tabs
-            tabs={tabs}
-            activeTab={activeTabId}
-            onTabSelected={setActiveTabId}
-          />
-        </div>
-      </div>
-    )
-  };
+  return (
+    <div className="section">
+      <Tabs
+        tabs={tabs}
+        activeTab={activeTabId}
+        onTabSelected={setActiveTabId}
+      />
+    </div>
+  );
+};
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,36 +1,40 @@
-export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
-  <>
-    <div className="tabs is-boxed">
-      <ul>
-        {
-          tabs.map(tab => {
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  return (
+    <>
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
 
-            return (
+      <div className="tabs is-boxed">
+        <div data-cy="TabsComponent">
 
-              <li
-                key={tab.id}
-                className={tab.id === activeTabId ? 'is-active' : ''}
-                data-cy="Tab">
-                <a
-                  href={`#${tab.id}`}
-                  data-cy="TabLink"
-                  onClick={
-                    () => {
-                      if (tab.id !== activeTabId) {
+          <ul>
+            {tabs.map(tab => {
+              return (
+                <li
+                  key={tab.id}
+                  className={tab.id === activeTab.id ? 'is-active' : ''}
+                  data-cy="Tab"
+                >
+                  <a
+                    href={`#${tab.id}`}
+                    data-cy="TabLink"
+                    onClick={() => {
+                      if (tab.id !== activeTab.id) {
                         onTabSelected(tab.id);
                       }
                     }}
-                >
-                  {tab.title}
-                </a>
-              </li>
-            )
-          })}
-
-      </ul>
-    </div >
-    <div className="block" data-cy="TabContent">
-      {activeTabId.content}
-    </div>
-  </>
-);
+                  >
+                    {tab.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </>
+  );
+};
